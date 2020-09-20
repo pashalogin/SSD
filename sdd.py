54ed51080eb6d114ff46abd93c84ec0c17b83de7
Ubuntu <ubuntu@ip-100-70-174-248.atlas.npr.aws.asurion.net>
2020-09-20 04:02:25 +0000

    Code Cleanup

--- sdd.py
+++ sdd.py
@@ -9,19 +9,20 @@ from Selector import *
 BASE_DIR = '/home/ubuntu/flaskapp'
 db_path = os.path.join(BASE_DIR, "ex1.db")
 
-def insert(db, val1, val2):
-    db.execute("insert into tbl1 values('{}','{}')".format(val1,val2))
-    cursor = db.execute("select * from tbl1");
-    for row in cursor:
-         print(row[0])
-
 def createTables(db):
     db.execute("DROP TABLE ZIP_SLOT_SIM")
     db.execute("DROP TABLE SLOT_CAPACITY")
     db.execute("CREATE TABLE ZIP_SLOT_SIM(ZIPCODE INT, MARKET TEXT,SLOT INT, CLAIM_CNT INT,SERVED_FLAG INT, DENSITY_PRED INT, DENSITY_SUCC INT)")
     db.execute("CREATE TABLE SLOT_CAPACITY(MARKET TEXT, DAY_OF_WK INT, SLOT INT, CAPACITY, CAPACITY_REMAINING)")
 
-def loadData(db):
+def loadDFs():
+    global data
+    data = pd.read_csv('data/HackathonInputData.txt', delimiter="|")
+    zipInfo = pd.read_csv('data/zip_info_STATIC_v2.csv', delimiter=",")
+    slotInfo = pd.read_csv('data/zip_slot_info_STATIC_v1.csv', delimiter=",")
+   # print(data.SERVICE_JOB_ID)
+
+def loadDynamicData(db):
     pd.read_csv('data/zip_slot_sim_DYNAMIC_v1.csv', delimiter=",").to_sql("ZIP_SLOT_SIM",db, if_exists='append', index=False)
     displayData(db, "ZIP_SLOT_SIM")
     pd.read_csv('data/slot_capacity_DYNAMIC_full_v1.csv', delimiter=",").to_sql("SLOT_CAPACITY",db, if_exists='append', index=False)
@@ -35,6 +36,12 @@ def displayData(db, tableName):
 
 def main():
     loadDFs()
+    
+    with sqlite3.connect(db_path) as db:
+       #createTables(db)
+       # insert(db,"pasha",2000)
+       loadDynamicData(db)
+    db.close()
 
     slotSelector = SlotSelector()
     slots = {
@@ -52,25 +59,16 @@ def main():
     print(slotSelector.choose_slot(slots,availability))
     print('Done')
 
-     #db.execute('create table tbl1(one varchar(10), two smallint)');
-    
-    with sqlite3.connect(db_path) as db:
-       createTables(db)
-       # insert(db,"pasha",2000)
-       loadData(db)
-    db.close()
-
-def loadDFs():
-    global data
-    data = pd.read_csv('data/HackathonInputData.txt', delimiter="|")
-    zipInfo = pd.read_csv('data/zip_info_STATIC_v2.csv', delimiter=",")
-    slotInfo = pd.read_csv('data/zip_slot_info_STATIC_v1.csv', delimiter=",")
-   # print(data.SERVICE_JOB_ID)
-
 def costFunction():
     print("*************")
     print(data)
     print("*************")
+
+def insert(db, val1, val2):
+    db.execute("insert into tbl1 values('{}','{}')".format(val1,val2))
+    cursor = db.execute("select * from tbl1");
+    for row in cursor:
+         print(row[0])
 
 if __name__=="__main__": 
     main()
